1f459c449d7ed2b7a1a431e7105c1d6ea6a863de
ronniefrias <ronnie.frias@gmail.com>
2022-04-30 22:02:33 -0500

    Cart Page Part 1

---
+++ ui-testing/selenium-java/src/test/java/pageobjects/SauceDemoCartPage.java
@@ -0,0 +1,32 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SauceDemoCartPage extends AbstractPage {
+
+    public final static By shoppingCart = By.className("shopping_cart_link");
+    public final static By pageHeader = By.className("title");
+    public final static By checkout = By.id("checkout");
+
+    public SauceDemoCartPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getHeaderText() {
+        getWait().until(ExpectedConditions.visibilityOfElementLocated(pageHeader));
+        return getDriver().findElement(pageHeader).getText();
+    }
+
+    public boolean confirmAdditionToCart(String item) {
+        By removeFromCart = By.id("remove-sauce-labs-" + item);
+        try {
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(removeFromCart));
+            return true;
+        }
+        catch(Exception e) {
+            return false;
+        }
+    }
+}
