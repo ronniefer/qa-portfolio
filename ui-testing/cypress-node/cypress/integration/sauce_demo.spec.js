c86eaa2b09fa4a4dd1c55c94ca909b689b0b4437
ronniefrias <ronnie.frias@gmail.com>
2022-06-07 22:51:07 -0500

    Single Run spec file

---
+++ ui-testing/cypress-node/cypress/integration/sauce_demo.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+it('should navigate and shop the Sauce Demo site', () => {
+    cy.visit('https://www.saucedemo.com/')
+    
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    cy.get('.title').should('have.text','Products') 
+    cy.get('#add-to-cart-sauce-labs-backpack').click()
+    cy.get('#add-to-cart-sauce-labs-onesie').click()
+    cy.get('.shopping_cart_link').click()
+
+    cy.get('.title').should('have.text','Your Cart') 
+    cy.get('#remove-sauce-labs-backpack').should('be.visible')
+    cy.get('#remove-sauce-labs-onesie').should('be.visible')
+    cy.get('#remove-sauce-labs-backpack').click()
+    cy.get('#remove-sauce-labs-backpack').should('not.be.visible')
+    cy.get('#checkout').click()
+    
+    cy.get('#first-name').type('John')
+    cy.get('#last-name').type('Smith')
+    cy.get('#postal-code').type('78701')
+    cy.get('#continue').click()
+
+    cy.get('#finish').click()
+    cy.get('#back-to-products').click()
+})
