6dec4866ba014988d1b20df204446d0024c2c51f
ronniefrias <ronnie.frias@gmail.com>
2022-06-12 22:59:04 -0500

    Login Page Part 2

--- ui-testing/cypress-node/cypress/page-objects/sauce-demo-login-page.js
+++ ui-testing/cypress-node/cypress/page-objects/sauce-demo-login-page.js
@@ -4,4 +4,16 @@ export class SauceDemoLoginPage {
     loadPage() {
         cy.visit('https://www.saucedemo.com')
     }
+
+    enterCredentials(username, password) {
+        cy.get('#user-name').should('be.visible')
+            .type(username)
+        cy.get('#password')
+            .type(password, { log:false })
+    }
+
+    clickLoginButton() {
+        cy.get('#login-button').should('be.visible')
+            .click()
+    }
 }
